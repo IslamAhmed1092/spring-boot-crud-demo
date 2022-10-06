6812fa1820c44f79ae360d4165f4c64b99cb7ab9
IslamAhmed1092 <eslam1092@hotmail.com>
2022-10-06 14:20:59 +0200

    create dao method to return all employees from the database

---
+++ src/main/java/com/example/springboot/cruddemo/dao/EmployeeDaoHibernateImpl.java
@@ -0,0 +1,33 @@
+package com.example.springboot.cruddemo.dao;
+
+import com.example.springboot.cruddemo.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class EmployeeDaoHibernateImpl implements EmployeeDao {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDaoHibernateImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        Query<Employee> query = currentSession.createQuery("from employee", Employee.class);
+
+
+        return query.getResultList();
+    }
+}

02a2d1606b7c775f6e16827bcb4a50f24f58753e
IslamAhmed1092 <eslam1092@hotmail.com>
2022-10-06 15:04:23 +0200

    add rest controller methods to update and delete employees from the database

--- src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
@@ -44,4 +44,23 @@ public class EmployeeRestController {
 
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted Employee id - " + employeeId;
+    }
 }

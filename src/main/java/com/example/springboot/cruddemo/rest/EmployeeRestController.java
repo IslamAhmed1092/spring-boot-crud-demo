d801eb6430056958d75cd190b200b1d4b3c5a3ec
IslamAhmed1092 <eslam1092@hotmail.com>
2022-10-06 14:26:30 +0200

    create rest controller with a method to get all employees from /employees endpoint

---
+++ src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.example.springboot.cruddemo.rest;
+
+
+import com.example.springboot.cruddemo.dao.EmployeeDao;
+import com.example.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDao.findAll();
+    }
+}
